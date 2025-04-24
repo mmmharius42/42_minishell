7ab6094b413b8f0bf1164841978147b5fe023a01
mmmharius <mariuspapin@rcmm.fr>
2025-04-24 20:25:37 +0200

    env copy on list

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -12,7 +12,93 @@
 
 #include "minishell.h"
 
-void    ft_env(t_env *env);
+t_env	*env_new_var(char *name, char *value, int equal_sign)
 {
+	t_env	*new_var;
 
+	new_var = malloc(sizeof(t_env));
+	if (new_var == NULL)
+		return (NULL);
+	if (name != NULL)
+		new_var->name = strdup(name);
+	else
+		new_var->name = NULL;
+	if (value != NULL)
+		new_var->value = strdup(value);
+	else
+		new_var->value = NULL;
+	new_var->equal_sign = equal_sign;
+	new_var->next = NULL;
+	return (new_var);
+}
+
+char	*get_name(char *str)
+{
+	int		i;
+	char	*name;
+
+	i = 0;
+	while (str[i] != '\0' && str[i] != '=')
+		i++;
+	name = malloc(i + 1);
+	if (name == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '=')
+	{
+		name[i] = str[i];
+		i++;
+	}
+	name[i] = '\0';
+	return (name);
+}
+
+char	*get_value(char *str)
+{
+	char	*equal;
+	char	*value;
+
+	equal = ft_strchr(str, '=');
+	if (equal == NULL)
+		return (NULL);
+	value = ft_strdup(equal + 1);
+	return (value);
+}
+
+void	env_add_back(t_env **env, t_env *new)
+{
+	t_env	*tmp;
+
+	if (env == NULL || new == NULL)
+		return ;
+	if (*env == NULL)
+	{
+		*env = new;
+		return ;
+	}
+	tmp = *env;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+
+void	env_init(t_env **env_list, char **env)
+{
+	int		i;
+	char	*name;
+	char	*value;
+	int		equal;
+	t_env	*new;
+
+	i = -1;
+	while (env[++i] != NULL)
+	{
+		equal = (ft_strchr(env[i], '=') != NULL);
+		name = get_name(env[i]);
+		value = get_value(env[i]);
+		new = env_new_var(name, value, equal);
+		free(name);
+		free(value);
+		env_add_back(env_list, new);
+	}
 }
